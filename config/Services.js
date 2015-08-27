4c09c5de489e871881002ba44502c079000c2ed0
danielefenix <daniele.salvatore@fao.org>
2015-08-27 16:01:36 +0200

    Bug fix: donwload section year selector refresh

--- config/Services.js
+++ config/Services.js
@@ -11,7 +11,7 @@ define(function ( ) {
 
         REFRESH_INDICATOR : "select p as parent,c as code, l as label, s as source from indicatorsList(string_to_array('{qualifier}',','), string_to_array('{country}',','), string_to_array('{year}',','))",
         REFRESH_QUALIFIER : "select p as parent,c as code, l as label from qualifiersList(string_to_array('{indicator}',','), string_to_array('{country}',','), string_to_array('{year}',','))",
-        REFRESH_YEAR : "select y as code, y as label from yearsList(string_to_array('{year}',','),string_to_array('{qualifier}',','),string_to_array('{country}',',')) order by label",
+        REFRESH_YEAR : "select y as code, y as label from yearsList(string_to_array('{indicator}',','),string_to_array('{qualifier}',','),string_to_array('{country}',',')) order by label",
         REFRESH_COUNTRY : "select c as code, l as label from countriesList(string_to_array('{indicator}',','), string_to_array('{qualifier}',','), string_to_array('{year}',','))",
 
         DOWNLOAD_SEARCH : "select  'Indicator' as Indicator, 'Indicator_label' as Indicator_label, 'Source'  as Source,'Source_label' as Source_label,'Country' as Country,'Country_label' as Country_label,'Year' as Year,'Year_label' as Year_label,'Qualifier' as Qualifier,'Qualifier_label' as Qualifier_label,'Value' as Value,'Unit' as Unit,'Flag' as Flag union all select Indicator,Indicator_label,Source,Source_label,Country,Country_label,Year,Year_label,Qualifier,Qualifier_label,Value,Unit,Flag from masterDataSelection( string_to_array('{indicator}',','), string_to_array('{country}',','), string_to_array('{year}',','), string_to_array('{qualifier}',',') )"
